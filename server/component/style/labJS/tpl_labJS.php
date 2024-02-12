40ecec18b8717eeb1a14b9a53ea3c52dbc77ae97
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-02-12 17:30:01 +0100

    add css to load the experiment with descent height

--- server/component/style/labJS/tpl_labJS.php
+++ server/component/style/labJS/tpl_labJS.php
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
 <div class="selfHelp-lab-js-holder <?php echo $this->css; ?>" data-lab-js='<?php echo isset($this->lab['content']) ?  htmlspecialchars($this->lab['content'], ENT_QUOTES, 'UTF-8') : ""; ?>' data-lab-js-fields='<?php echo isset($lab_fields) ? $lab_fields : "" ?>'>
-    <div class="selfHelp-lab-js" data-labjs-section="main">
+    <div class="selfHelp-lab-js container fullscreen" data-labjs-section="main">
         
     </div>
 </div>
