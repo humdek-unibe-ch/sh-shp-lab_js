8147317821dae6be7a17e428edf12ece44189d76
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-02-14 15:59:38 +0100

    load labj justified in center

--- server/component/style/labJS/tpl_labJS.php
+++ server/component/style/labJS/tpl_labJS.php
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<div class="selfHelp-lab-js-holder <?php echo $this->css; ?>" data-lab-js='<?php echo isset($this->lab['content']) ?  htmlspecialchars($this->lab['content'], ENT_QUOTES, 'UTF-8') : ""; ?>' data-lab-js-fields='<?php echo isset($lab_fields) ? $lab_fields : "" ?>'>
+<div class="selfHelp-lab-js-holder d-flex justify-content-center <?php echo $this->css; ?>" data-lab-js='<?php echo isset($this->lab['content']) ?  htmlspecialchars($this->lab['content'], ENT_QUOTES, 'UTF-8') : ""; ?>' data-lab-js-fields='<?php echo isset($lab_fields) ? $lab_fields : "" ?>'>
     <div class="selfHelp-lab-js container fullscreen" data-labjs-section="main">
         
     </div>
