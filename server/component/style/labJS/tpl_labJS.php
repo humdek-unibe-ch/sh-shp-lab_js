bbc601322a2c94821bc2556a41f26b9ef193373f
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-11-27 10:34:07 +0100

    properly load labjs fields

--- server/component/style/labJS/tpl_labJS.php
+++ server/component/style/labJS/tpl_labJS.php
@@ -3,7 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<div class="selfHelp-lab-js-holder d-flex justify-content-center <?php echo $this->css; ?>" data-lab-js='<?php echo $this->lab_config ?>'>
+<div class="selfHelp-lab-js-holder d-flex justify-content-center 
+<?php echo $this->css; ?>" data-lab-js='<?php echo $this->lab_config ?>'
+    data-lab-js-fields='<?php echo isset($lab_fields) ? $lab_fields : "" ?>'>
     <div class="selfHelp-lab-js container fullscreen" data-labjs-section="main">
 
     </div>
